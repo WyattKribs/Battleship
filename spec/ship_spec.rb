e7949e429b7b4872a5ae3f41966506f7221281ae
Wyatt Kribs <kribswyatt@gmail.com>
2022-05-17 11:38:39 -0600

    Adds complete methods and tests for ship class

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -16,5 +16,31 @@ describe Ship do
   it "has a length" do
     expect(@cruiser.length).to eq(3)
   end
-  
+
+  it "has health" do
+    expect(@cruiser.health).to eq(3)
+  end
+
+  it "isn't sunk by default" do
+    expect(@cruiser.sunk?).to eq(false)
+  end
+
+  it "can be hit" do
+    @cruiser.hit
+    expect(@cruiser.health).to eq(2)
+  end
+
+  it "can be hit multiple times" do
+    @cruiser.hit
+    @cruiser.hit
+    expect(@cruiser.health).to eq(1)
+  end
+
+  it "can be sunk" do
+    @cruiser.hit
+    @cruiser.hit
+    @cruiser.hit
+    expect(@cruiser.health).to eq(0)
+    expect(@cruiser.sunk?).to eq(true)
+  end
 end
