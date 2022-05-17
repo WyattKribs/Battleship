23335edc5b22b649d68aec4995e859cd8295770f
Wesley Miranda <wesleywes42@gmail.com>
2022-05-17 14:50:58 -0600

    Adds empty and place ship methods

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,10 +1,22 @@
 class Cell
-attr_reader :coordinate
+attr_reader :coordinate, :ship
 
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = nil
   end
 
+  def empty?
+    if ship == nil
+      true
+    else
+      false
+    end
+  end
+
+  def place_ship(ship_name)
+    @ship = ship_name
+  end
 
 
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -5,6 +5,7 @@ require './lib/cell'
 describe Cell do
   before(:each) do
     @cell = Cell.new("B4")
+    @cruiser = Ship.new("Cruiser", 3)
   end
 
   it "is an instance of cell" do
@@ -14,6 +15,26 @@ describe Cell do
   it "has coordinates" do
     expect(@cell.coordinate).to eq("B4")
   end
+
+  it "cell has no ship by default " do
+
+  expect(@cell.ship).to eq(nil)
+  end
+
+  it "cell is empty" do
+    expect(@cell.empty?).to eq(true)
+  end
+
+  it "Can place a ship" do
+    @cell.place_ship(@cruiser)
+    expect(@cell.ship).to eq(@cruiser)
+  end
+
+  it "Tell us the cell has a ship" do
+    @cell.place_ship(@cruiser)
+    expect(@cell.empty?).to eq(false)
+  end
+
 
 
 end
