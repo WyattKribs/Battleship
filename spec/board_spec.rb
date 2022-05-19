95d63a84c27ec9b7304fbcc331923c53bf0537de
Wyatt Kribs <kribswyatt@gmail.com>
2022-05-19 14:31:11 -0600

    Board class and spec created

---
+++ spec/board_spec.rb
@@ -0,0 +1,16 @@
+require './lib/board'
+
+describe Board do
+  before(:each) do
+    @board = Board.new
+  end
+
+  it "is an instance of Board" do
+    expect(@board).to be_a(Board)
+  end
+
+  it "can verify coordinates" do
+    expect(@board.valid_coordinate?("A1")).to eq(true)
+    # expect(@board.valid_coordinate?("A11")).to eq(false)
+  end
+end
