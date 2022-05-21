ef016007eb8ce7c62d30fe09a1ba862ada28b585
Wesley Miranda <wesleywes42@gmail.com>
2022-05-21 11:40:08 -0600

    Check coordinates can’t be diagonal

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -31,6 +31,9 @@ describe Board do
     expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to eq(false)
   end
 
-  
+  it "coordinates can’t be diagonal" do
+    expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to eq(false)
+    expect(@board.valid_placement?(@submarine, ["C2", "D3"])).to eq(false)
+  end
 
 end
