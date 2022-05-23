8df1e1e85173fb4b476d51eae3b2a4e923ee07ab
Wyatt Kribs <kribswyatt@gmail.com>
2022-05-23 14:27:52 -0600

    adds more functionality to runner

--- battleship.rb
+++ battleship.rb
@@ -10,12 +10,16 @@ p 'type p to play or q to quit'
 end
 
 def create_game
-  play_board = Board.new
-  comp_board = Board.new
-  play_cruiser = Ship.new("Cruiser", 3)
-  play_submarine = Ship.new("Submarine", 2)
-  comp_cruiser = Ship.new("Cruiser", 3)
-  comp_submarine = Ship.new("Submarine", 2)
+  @play_board = Board.new
+  @comp_board = Board.new
+  @play_cruiser = Ship.new("Cruiser", 3)
+  @play_submarine = Ship.new("Submarine", 2)
+  @comp_cruiser = Ship.new("Cruiser", 3)
+  @comp_submarine = Ship.new("Submarine", 2)
+end
+
+def comp_placement
+
 end
 
 def the_game
@@ -24,7 +28,22 @@ def the_game
     play_or_quite = gets.chomp.upcase
     if play_or_quite == 'P'
       create_game
-
+      p 'place your ships!'
+      puts @play_board.render
+      loop do
+        p 'place the cruiser, pick 3 consecutive positions'
+        cruiser_place = []
+        3.times do
+          cruiser_place << gets.chomp.upcase
+        end
+        if @play_board.valid_placement?(@play_cruiser, cruiser_place) == true
+          @play_board.place(@play_cruiser, cruiser_place)
+          break
+        else
+          p 'invalid placement, pick again'
+        end
+      end
+      puts @play_board.render(true)
     elsif play_or_quite == 'Q'
       break
     else
